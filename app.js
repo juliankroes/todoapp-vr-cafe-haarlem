5f53bf5bd3ead6f4386bb2d252f230c50407fdfe
julian <juliankroes2206@gmail.com>
2025-05-30 14:00:11 +0200

    feat: todo's kunnen worden voltooid en verwijderd

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const LOCAL_STORAGE_TODO_KEY = "todolist"
 
-
-
 const textInput = document.getElementById("textInput")
 const categoryDropdown = document.getElementById("categoryDropdown")
 const submitButton = document.getElementById("submitTodo")
@@ -27,6 +25,7 @@ submitButton.addEventListener("click", () => {
     }
 
     todo = {
+        id: crypto.randomUUID(),
         todoItem: textInput.value,
         category: categoryDropdown.value,
         creationTime: Date.now(),
@@ -40,7 +39,7 @@ submitButton.addEventListener("click", () => {
 
 function localSave(todo) {
     const todoArrayString = localStorage.getItem(LOCAL_STORAGE_TODO_KEY)
-    let todoArray = JSON.parse(todoArrayString);
+    let todoArray = JSON.parse(todoArrayString)
     todoArray.push(todo)
     const updatedTodoArrayString = JSON.stringify(todoArray)
     localStorage.setItem(LOCAL_STORAGE_TODO_KEY, updatedTodoArrayString)
@@ -48,9 +47,37 @@ function localSave(todo) {
 
 function getAllTodos() {
     const todoArrayString = localStorage.getItem(LOCAL_STORAGE_TODO_KEY)
-    const todoArray = JSON.parse(todoArrayString);
+    const todoArray = JSON.parse(todoArrayString)
     return todoArray
 }
+
+function deleteTodoById(id) {
+    let todoList = getAllTodos()
+    todoList = todoList.filter(todo => todo.id !== id)
+    const updatedTodoArrayString = JSON.stringify(todoList)
+    localStorage.setItem(LOCAL_STORAGE_TODO_KEY, updatedTodoArrayString)
+    loadTodos()
+}
+
+function completeTodoById(id) {
+    let todoList = getAllTodos()
+    
+    // hier doen we gewoon een linear search omdat er realistisch gezien nooit meer dan 20 todo's zullen zijn 
+    for (todo of todoList) {
+        if (todo.id === id && !todo.completed) {
+            todo.completed = true;
+        } else if (todo.id === id && todo.completed) {
+            deleteTodoById(id)
+            return
+        }
+
+    }
+
+    const updatedTodoArrayString = JSON.stringify(todoList)
+    localStorage.setItem(LOCAL_STORAGE_TODO_KEY, updatedTodoArrayString)
+    loadTodos()
+}
+
 
 function loadTodos() {
     todoList = document.getElementById("todoList")
@@ -63,7 +90,11 @@ function loadTodos() {
         // create li, add to div
         todoItem = document.createElement('li')
         todoItem.innerText = todo.todoItem;
-        todoItem.className = "todo-item"
+        todoItem.className = 'todo-item'
+        if (todo.completed) {
+            todoItem.className += ' completed'
+        }
+        todoItem.addEventListener('click', () => {completeTodoById(todo.id)})
 
         inlineItem.appendChild(todoItem)
 
